5ada10beadf28d3f9344a622f8b35a0355e08f8f
Miguel Fermin <mfermin@mafsoftware.com>
2022-05-28 09:02:58 -0400

    renamed method

--- Sources/HttpServer/Handler.swift
+++ Sources/HttpServer/Handler.swift
@@ -7,6 +7,7 @@
 
 public typealias Next = (Any...) -> Void
 public typealias HandleFunc = (Request, ResponseWriter, @escaping Next) -> Void
+public typealias HandleFuncAsync<O> = (Request) async throws -> O
 
 protocol Handler {
     func handle(request: Request, response: ResponseWriter, next upperNext: @escaping Next)
